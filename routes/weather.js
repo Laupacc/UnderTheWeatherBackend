a481b079bc8e0c6d114c7fa5d342ecf6683dcce1
Your name <Your email>
2024-05-14 14:50:48 +0200

    l

--- routes/weather.js
+++ routes/weather.js
@@ -59,41 +59,80 @@ router.post('/current', (req, res) => {
 });
 
 // Add city from user location
-router.post('/current/location', (req, res) => {
-	City.findOne({ cityName: { $regex: new RegExp(req.body.cityName, 'i') } }).then(dbData => {
-		// City.findOne({ lattitude: req.body.lat, longitude: req.body.lon }).then(dbData => {
-		if (dbData === null) {
-			fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&appid=${OWM_API_KEY}&units=metric`)
-				.then(response => response.json())
-				.then(apiData => {
-					const newCity = new City({
-						cityName: apiData.name,
-						main: apiData.weather[0].main,
-						description: apiData.weather[0].description,
-						icon: apiData.weather[0].icon,
-						temp: apiData.main.temp,
-						feels_like: apiData.main.feels_like,
-						tempMin: apiData.main.temp_min,
-						tempMax: apiData.main.temp_max,
-						humidity: apiData.main.humidity,
-						wind: apiData.wind.speed,
-						clouds: apiData.clouds.all,
-						rain: apiData.rain ? apiData.rain['1h'] : 0,
-						snow: apiData.snow ? apiData.snow['1h'] : 0,
-						sunrise: apiData.sys.sunrise,
-						sunset: apiData.sys.sunset,
-						lattitude: apiData.coord.lat,
-						longitude: apiData.coord.lon,
-					});
-					newCity.save().then(newDoc => {
-						res.json({ result: true, weather: newDoc });
-					});
-				});
-		} else {
+// router.post('/current/location', (req, res) => {
+// 	City.findOne({ cityName: { $regex: new RegExp(req.body.cityName, 'i') } }).then(dbData => {
+// 		// City.findOne({ lattitude: req.body.lat, longitude: req.body.lon }).then(dbData => {
+// 		if (dbData === null) {
+// 			fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&appid=${OWM_API_KEY}&units=metric`)
+// 				.then(response => response.json())
+// 				.then(apiData => {
+// 					const newCity = new City({
+// 						cityName: apiData.name,
+// 						main: apiData.weather[0].main,
+// 						description: apiData.weather[0].description,
+// 						icon: apiData.weather[0].icon,
+// 						temp: apiData.main.temp,
+// 						feels_like: apiData.main.feels_like,
+// 						tempMin: apiData.main.temp_min,
+// 						tempMax: apiData.main.temp_max,
+// 						humidity: apiData.main.humidity,
+// 						wind: apiData.wind.speed,
+// 						clouds: apiData.clouds.all,
+// 						rain: apiData.rain ? apiData.rain['1h'] : 0,
+// 						snow: apiData.snow ? apiData.snow['1h'] : 0,
+// 						sunrise: apiData.sys.sunrise,
+// 						sunset: apiData.sys.sunset,
+// 						lattitude: apiData.coord.lat,
+// 						longitude: apiData.coord.lon,
+// 					});
+// 					newCity.save().then(newDoc => {
+// 						res.json({ result: true, weather: newDoc });
+// 					});
+// 				});
+// 		} else {
+// 			res.json({ result: false, error: 'City already saved' });
+// 		}
+// 	});
+// });
+
+router.post('/current/location', async (req, res) => {
+	try {
+		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&appid=${OWM_API_KEY}&units=metric`);
+		const apiData = await response.json();
+
+		const existingCity = await City.findOne({ cityName: apiData.name });
+		if (existingCity) {
 			res.json({ result: false, error: 'City already saved' });
+			return;
 		}
-	});
+
+		const newCity = new City({
+			cityName: apiData.name,
+			main: apiData.weather[0].main,
+			description: apiData.weather[0].description,
+			icon: apiData.weather[0].icon,
+			temp: apiData.main.temp,
+			feels_like: apiData.main.feels_like,
+			tempMin: apiData.main.temp_min,
+			tempMax: apiData.main.temp_max,
+			humidity: apiData.main.humidity,
+			wind: apiData.wind.speed,
+			clouds: apiData.clouds.all,
+			rain: apiData.rain ? apiData.rain['1h'] : 0,
+			snow: apiData.snow ? apiData.snow['1h'] : 0,
+			sunrise: apiData.sys.sunrise,
+			sunset: apiData.sys.sunset,
+			lattitude: apiData.coord.lat,
+			longitude: apiData.coord.lon,
+		});
+
+		const newDoc = await newCity.save();
+		res.json({ result: true, weather: newDoc });
+	} catch (error) {
+		res.status(500).json({ result: false, error: 'Internal Server Error' });
+	}
 });
+
 
 
 
