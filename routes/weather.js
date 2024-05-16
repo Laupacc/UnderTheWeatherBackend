4a42cb843d119cb67e54f66ecf0c0ace6fa968c1
Your name <Your email>
2024-05-16 16:49:12 +0200

    c

--- routes/weather.js
+++ routes/weather.js
@@ -169,18 +169,24 @@ router.put("/:cityName", (req, res) => {
 });
 
 // fetch citynames for autocomplete search bar
+
 router.get('/cityautocomplete', async (req, res) => {
 	try {
 		const response = await fetch('https://countriesnow.space/api/v0.1/countries');
 		const apiData = await response.json();
-		const cities = apiData.data.map(country => country.cities).flat();
+		console.log(apiData);
+
+		// Extract all cities from the API response
+		const cities = apiData.data.reduce((acc, country) => {
+			acc.push(...country.cities);
+			return acc;
+		}, []);
 
 		res.json({ result: true, cities: cities });
 	} catch (error) {
 		res.json({ result: false, error: 'Internal Server Error' });
 	}
 });
-
 
 
 
