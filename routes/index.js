ca70f1dffdd97deccb09fdcf33ce57959de40768
Your name <Your email>
2024-05-16 18:11:28 +0200

    e

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,26 @@ var router = express.Router();
 
 const fetch = require('node-fetch');
 
+// Get all cities from API for autocomplete feature in frontend header
 router.get('/cityautocomplete', async (req, res) => {
-	try {
-		const response = await fetch('https://countriesnow.space/api/v0.1/countries');
-		if (!response.ok) {
-			res.json({ result: false, error: 'An error occurred while fetching the city names' });
-			return;
-		}
-		const apiData = await response.json();
+    try {
+        const response = await fetch('https://countriesnow.space/api/v0.1/countries');
+        if (!response.ok) {
+            res.json({ result: false, error: 'An error occurred while fetching the city names' });
+            return;
+        }
+        const apiData = await response.json();
         const cities = apiData.data.map((country) => country.cities).flat();
+        const isoCodes = apiData.data.map((country) => country.iso2);
+        cities.forEach((city, index) => {
+            city.iso2 = isoCodes[index];
+        });
 
-		res.json({ result: true, cities: cities });
-	} catch (error) {
-		console.error(error);
-		res.json({ result: false, error: 'Internal Server Error' });
-	}
+        res.json({ result: true, cities: cities });
+    } catch (error) {
+        console.error(error);
+        res.json({ result: false, error: 'Internal Server Error' });
+    }
 });
 
 module.exports = router;
