c6bbf09b0422eaf511fc3d77eadd6fb536beaaff
Your name <Your email>
2024-05-20 10:18:48 +0100

    l

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,8 @@ var router = express.Router();
 
 const fetch = require('node-fetch');
 
-// Get all cities from API for autocomplete feature in frontend header
-// router.get('/cityautocomplete', async (req, res) => {
-//     try {
-//         const response = await fetch('https://countriesnow.space/api/v0.1/countries');
-//         if (!response.ok) {
-//             res.json({ result: false, error: 'An error occurred while fetching the city names' });
-//             return;
-//         }
-//         const apiData = await response.json();
-
-//         let cities = [];
-//         apiData.data.forEach((country) => {
-//             const countryCode = country.iso2;
-//             const countryCities = country.cities;
-
-//             const countryCitiesMapped = countryCities.map((city) => {
-//                 return { name: city, iso2: countryCode };
-//             });
-
-//             cities = [...cities, ...countryCitiesMapped];
-//         });
-
-//         // Sort cities alphabetically
-//         cities.sort((a, b) => a.name.localeCompare(b.name));
-
-//         res.json({ result: true, cities: cities });
-//     } catch (error) {
-//         console.error(error);
-//         res.json({ result: false, error: 'Internal Server Error' });
-//     }
-// });
-
-
 // Get all cities from API for autocomplete feature
-router.get('/cityautocomplete', async (req, res) => {
+router.get('/cityAutoFeature', async (req, res) => {
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/find&appid=${OWM_API_KEY}&units=metric`);
         if (!response.ok) {
