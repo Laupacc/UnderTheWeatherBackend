d2c83b4d9a5bb2085ced10cdfc9e058fddaf82f6
Your name <Your email>
2024-05-25 15:26:24 +0200

    r

--- models/users.js
+++ models/users.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     username: String,
     password: String,
     token: String,
-    cities: [{ type: Schema.Types.ObjectId, ref: 'City' }],
+    // cities: [{ type: Schema.Types.ObjectId, ref: 'City' }],
 });
 
 const User = mongoose.model('users', userSchema);
