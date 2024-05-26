e456612623c07cda1eefbfcd7b9216a5766a41cd
Your name <Your email>
2024-05-26 15:03:26 +0200

    vv

--- models/users.js
+++ models/users.js
@@ -4,7 +4,27 @@ const userSchema = mongoose.Schema({
     username: { type: String, unique: true },
     password: { type: String, required: true },
     token: { type: String, required: true },
-    cities: [citySchema],
+    cities: [{
+        cityName: String,
+        country: String,
+        main: String,
+        description: String,
+        icon: String,
+        temp: Number,
+        feels_like: Number,
+        tempMin: Number,
+        tempMax: Number,
+        humidity: Number,
+        wind: Number,
+        clouds: Number,
+        rain: Number,
+        snow: Number,
+        sunrise: Number,
+        sunset: Number,
+        latitude: Number,
+        longitude: Number,
+        timezone: Number,
+    }],
 });
 
 const User = mongoose.model('users', userSchema);
